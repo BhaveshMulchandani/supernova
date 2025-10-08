ee4e91253fbaac1fde7020476cee176c23c20d92
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2025-10-08 23:29:36 +0530

    Implement product retrieval, update, and deletion features; add routes for fetching products by ID and seller

--- product/src/controllers/product.controller.js
+++ product/src/controllers/product.controller.js
@@ -53,5 +53,95 @@ const getproducts = async (req,res)=>{
 
 }
 
+const getproductsbyid = async (req,res)=>{
+    const {id} = req.params
 
-module.exports = { createproduct, getproducts }
+    const product = await productmodel.findById(id)
+
+    if(!product){
+        return res.status(404).json({message:"product not found"})
+    }
+
+    return res.status(200).json({message:"Product fetched successfully",product })
+
+
+}
+
+const updateproduct = async (req,res) => {
+    const {id} = req.params
+    
+    if(!mongoose.Types.ObjectId.isvalid(id)){
+        return res.status(400).json({message:"Invalid product ID"})
+    }
+
+    const product = await productmodel.findOne({_id:id})
+
+    if(!product){
+        return res.status(404).json({message:"Product not found"})
+    }
+
+    if(product.seller.toString() !== req.user.id){
+        return res.status(403).json({message:"You are not authorized to update this product"})
+    }
+
+    const allowedupdates = ['title', 'description', 'price'];
+
+    for (const key of Object.keys(req.body)){
+        if (allowedupdates.includes(key)) {
+            if(key === 'price' && typeof req.body.price === 'object') {
+                product.price.amount = Number(req.body.price.amount)
+            }
+            if(req.body.price.currency !== undefined){
+                product.price.currency = req.body.price.currency
+            }
+        }else{
+            product[key] = req.body[key]
+        }
+    }
+
+    await product.save()
+
+    return res.status(200).json({message:"Product updated successfully",product})
+}
+
+const deleteproduct = async (req,res) => {
+
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isvalid(id)){
+        return res.status(400).json({message:"Invalid product ID"})
+    }
+
+    const product = await productmodel.findOne({_id:id})
+
+    if(!product){
+        return res.status(404).json({message:"Product not found"})
+    }
+
+    if(product.seller.toString() !== req.user.id){
+        return res.status(403).json({message:"You are not authorized to delete this product"})
+    }
+
+    await productmodel.findOneAndDelete({_id:id})
+
+    return res.status(200).json({message:"Product deleted successfully"})
+
+
+
+
+}
+
+const getproductsbyseller = async (req,res) => {
+    const seller = req.user.id
+
+    const {skip=0,limit=20} = req.query;
+
+    const products = await productmodel.find({seller:seller.id}).skip(skip).limit(Math.min(Number(limit, 20)))
+
+    return res.status(200).json({message:"Products fetched successfully",products})
+
+
+
+}
+
+module.exports = { createproduct, getproducts, getproductsbyid, updateproduct, deleteproduct, getproductsbyseller }
--- product/src/routes/product.routes.js
+++ product/src/routes/product.routes.js
@@ -9,5 +9,10 @@ const upload = multer({storage: multer.memoryStorage()});
 
 router.post('/',createauthmiddleware(['admin','seller']), upload.array('image',5),createproductvalidators,Productcontroller.createproduct)
 router.get('/', Productcontroller.getproducts)
+router.patch('/:id',createauthmiddleware(["seller"]),Productcontroller.updateproduct)
+router.delete('/:id',createauthmiddleware(["seller"]),Productcontroller.deleteproduct)
+router.get('/seller',createauthmiddleware(['seller']),Productcontroller.getproductsbyseller)
+router.get('/:id',Productcontroller.getproductsbyid)
+
 
 module.exports = router;
